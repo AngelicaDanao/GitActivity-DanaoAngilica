7114f12e9b86de3d4dbea8051433b5341268286d
angelicamorongdanao@gmail.com <angelicamorongdanao@gmail.com>
2022-03-29 21:38:35 +0800

    My Educational Background added

--- git-python-danao.py
+++ git-python-danao.py
@@ -6,4 +6,8 @@ Created on Tue Mar 29 21:16:44 2022
 """
 print("Angelica M. Danao")
 print("Ino, Gasan, NArinduque")
-print("October 15 2000)
+print("October 15 2000")
+
+print("Marinduque State College")
+print("BS Information Technology")
+print("2E")
